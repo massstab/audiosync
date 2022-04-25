8aab7b4434f63ca0c814af8284df6f619332c2d3
massstab <dave@xiala.net>
2022-04-25 22:17:00 +0200

    Added annotation to triangle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # /usr/bin/python3
 
+import sys
+
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, FuncFormatter
 from matplotlib import animation
@@ -41,15 +43,11 @@ def base_plot(xmin, xmax):
     # ax.grid(visible=True, which='both', axis='both')
     for tic in ax.yaxis.get_major_ticks():
         tic.tick1line.set_visible(False)
-        tic.tick2line.set_visible(False)
         tic.label1.set_visible(False)
-        tic.label2.set_visible(False)
 
     modxmin = abs(xmin)
     ax.xaxis.set_major_locator(MultipleLocator(modxmin*1))
-    ax.yaxis.set_major_locator(MultipleLocator(modxmin*1))
     ax.xaxis.set_minor_locator(MultipleLocator(modxmin*0.1))
-    ax.yaxis.set_minor_locator(MultipleLocator(modxmin*0.1))
     ax.tick_params(axis='x', which='both', length=20, color='w', width=2, direction='in', pad=-36)
     ax.set_ylim(-0.5625*modxmin, 0.5625*modxmin)
     ax.set_aspect('equal')
@@ -58,8 +56,8 @@ def base_plot(xmin, xmax):
     return fig, ax
 
 def movie(fig, ax, xmin, xmax):
-    lw = 5
-    line, = plt.plot([], [], lw=lw, c='r', alpha=1)
+    lw = 2
+    line, = plt.plot([], [], lw=lw, c='r', alpha=0.6)
     v = np.array([
         [xmin-0.2*xmin, -0.2*abs(xmin)],
         [xmin, 0],
@@ -67,25 +65,41 @@ def movie(fig, ax, xmin, xmax):
     ])
     patch = patches.Polygon(v, closed=True, fc='r', ec='r')
     ax.add_patch(patch)
-
+    ann_list = []
     def init():
         line.set_data([], [])
         return line,
 
     def animate(i):
-        steps = 50
-        print(steps)
+        for a in ann_list:
+            a.remove()
+        ann_list[:] = []
+        steps = 200
+        # line
         x = np.linspace(xmin, xmax, steps+1)
-        y = [0, 0.2*abs(xmin)]
+        y = [0, 0.1*abs(xmin)]
         line.set_data([x[i],x[i]], y)
+
+        # triangle
         tmp = v.copy()
-        tmp[:, 0] += i*0.04*abs(xmin)
-        print(x[i])
+        tmp[:, 0] += i*0.01*abs(xmin)
         patch.set_xy(tmp)
+
+        # text
+        if tmp[1][0] < 0:
+            ann = ax.annotate(f'    {int(x[i]):04} ms\n Video ahead', [tmp[2][0]+5, tmp[2][1]-27], color='w', fontsize=10)
+        else:
+            ann = ax.annotate(f'     {int(x[i]):03} ms\n Audio ahead', [tmp[2][0]+5, tmp[2][1]-27], color='w', fontsize=10)
+        ann_list.append(ann)
+
+        # write animation progress
+        sys.stdout.write('\r')
+        sys.stdout.write("{0:.0f}%".format(i * 100 / steps))
+
         return line,
 
-    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=51, blit=True)
-    anim.save('editing/one_sync_cycle.mp4', fps=50, extra_args=['-vcodec', 'libx264'], bitrate=-1, dpi=240)
+    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=120, blit=True)
+    anim.save('editing/one_sync_cycle.mp4', fps=200, extra_args=['-vcodec', 'libx264'], bitrate=-1, dpi=240)
 
 
 if __name__ == '__main__':
